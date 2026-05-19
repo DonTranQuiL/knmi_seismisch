ca4a6c0fe3a71563d340a992af004a86fac67261
malosaaa <90527254+malosaaa@users.noreply.github.com>
2026-05-19 10:58:07 +0000

    style: auto-fix ruff linting and formatting [skip ci]

--- tests/test_config_flow.py
+++ tests/test_config_flow.py
@@ -8,8 +8,8 @@ from custom_components.knmi_seismisch.const import (
     CONF_INSTANCE_NAME,
     CONF_SEARCH_TERMS,
     CONF_SCAN_INTERVAL,
-    DEFAULT_SCAN_INTERVAL,
 )
+
 
 @pytest.fixture(autouse=True)
 def auto_enable_custom_integrations(enable_custom_integrations):
